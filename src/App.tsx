c3f61ccfbcaf3e72540ba70b8e52649591a54716
nidvar <djayshine87@hotmail.com>
2025-11-08 17:24:30 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
+                <input 
+                    type="email"
+                /><br /><br />
+                <input 
+                    type="password"
+                />
+            </div>
+        </>
+    )
+}
+
+export default App
