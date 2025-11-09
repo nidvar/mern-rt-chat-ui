893735d012fa44a72ef825ad44dffa12d077dec9
nidvar <djayshine87@hotmail.com>
2025-11-09 11:30:41 +1100

    login function working

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,70 @@
+import { useState } from 'react';
+
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 function App() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async function(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        if(email.trim() === '' || password.trim() === ''){
+            console.log('email / password is empty');
+            return;
+        };
+
+        const payload = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            credentials: 'include' as RequestCredentials,
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        };
+
+        const res = await fetch(baseURL + '/auth/login', payload);
+        const data = await res.json();
+        console.log(data);
+    };
+
     return (
         <>
             <div>
-                <h1>Login</h1>
-                <input 
-                    type="email"
-                /><br /><br />
-                <input 
-                    type="password"
-                />
+                <form onSubmit={handleSubmit}>
+                    <h1>Login</h1>
+
+                    <input 
+                        type="email"
+                        placeholder='email'
+                        value={email}
+                        onChange={function(e){setEmail(e.target.value)}}
+                    />
+
+                    <br />
+                    <br />
+
+                    <input 
+                        type="password"
+                        placeholder='password'
+                        onChange={function(e){setPassword(e.target.value)}}
+                    />
+
+                    <br />
+                    <br />
+
+                    <button type="submit">LOGIN</button>
+                </form>
+                <br />
+                <hr />
+                <br />
+                <button>LOGOUT</button>
             </div>
         </>
     )
 }
 
-export default App
+export default App;
