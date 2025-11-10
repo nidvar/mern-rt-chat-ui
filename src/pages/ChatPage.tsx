b1a92befa9b0aaf9bfc17bbca2b8a5b4cc854979
nidvar <djayshine87@hotmail.com>
2025-11-10 13:57:30 +1100

    logout function working

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,14 +1,20 @@
-// import { useState } from 'react';
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const ChatPage = function(){
 
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
+    const logout = async function(){
+        const payload = {
+            method: 'POST',
+            credentials: 'include' as RequestCredentials
+        }
+        await fetch(baseURL + '/auth/logout', payload);
+    }
 
     return(
         <>
             <div>
                 <h1>Chat Page</h1>
+                <button onClick={logout}>LOGOUT</button>
             </div>
         </>
     )
